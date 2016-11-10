5e01be4445286b691a03b66536a341257a9bdf18
Hou <avenger19970116@gmail.com>
2016-11-10 21:22:36 +0800

    test finished

--- 数据结构考试-LX/5.cpp
+++ 数据结构考试-LX/5.cpp
@@ -1,17 +1,16 @@
-//已知整型元素的线性表（a0, a1,a2,…,an-1）按顺序存储于一维数组A中，编写函数将各个元素向右移动k位 ：void movek（int A[], int n, int k）;即数据变为  an-k, …,an-1,a0,…,an-k-1
 #include <iostream>
 using namespace std;
 void movek(int a[], int n, int k) {
 	int *temp=new int[k];
-	for (int i = n - k, j = 0; i < n; ++i, ++j)
+	for (int i = 0; i < k; ++i)
 	{
-		temp[j] = a[i];
+		temp[i] = a[i];
 	}
-	for (int i = n - 1 - k, j = n - 1; i >= 0; --i, --j)
+	for (int i = 0, j = k; j < n; ++i, ++j)
 	{
-		a[j] = a[i];
+		a[i] = a[j];
 	}
-	for (int i = 0, j = 0; i < k; ++i, ++j)
+	for (int i = n-k, j = 0; i < n; ++i, ++j)
 	{
 		a[i] = temp[j];
 	}
@@ -23,7 +22,7 @@ int main(int argc, char const *argv[])
 	{
 		arr[i] = i;
 	}
-	movek(arr, 10, 7);
+	movek(arr, 10, 3);
 	for (int i = 0; i < 10; ++i)
 	{
 		cout << arr[i] << " ";
